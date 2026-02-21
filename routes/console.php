5839626a62d16cb583b3b9672030ee4352ae2e9a
Corey <admin@agstrategic.tech>
2026-02-20 21:38:04 -0500

    heartbeats

--- routes/console.php
+++ routes/console.php
@@ -3,4 +3,6 @@
 use App\Jobs\DispatchMarketplaceSearchesJob;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::job(new DispatchMarketplaceSearchesJob)->everyFifteenMinutes();
+Schedule::job(new DispatchMarketplaceSearchesJob)
+    ->everyFifteenMinutes()
+    ->pingOnSuccess(env('HEARTBEAT_DISPATCH_SEARCHES_URL'));
